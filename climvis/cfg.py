f824d1f1e4566eb75897c90dcc1b014ddf4a21fe
LAPTOP-LENOVO\Exi <birgit.bacher1988@gmail.com>
2018-11-29 19:32:30 +0100

    added function which reads datapath from configfile

--- climvis/cfg.py
+++ climvis/cfg.py
@@ -1,7 +1,31 @@
 """This configuration module is a container for parameters and constants."""
 import os
+import platform
+import configparser
 
-cru_dir = '/home/mowglie/disk/OGGM_INPUT/cru/'
+#cru_dir = '/home/mowglie/disk/OGGM_INPUT/cru/'
+
+def get_data_path():
+    """ Get data path from .cruvis file in home directory.
+    The file has to look like this:
+    [PATH]
+    cru_dir = /path/to/data/
+    """
+    homedir = os.path.expanduser('~')
+    # Get operating system and define path to .cruvis file
+    operatingsys = platform.platform()
+    if 'Windows' in operatingsys:
+        crufilepath = homedir + '\.cruvis'
+    elif 'Linux' in operatingsys:
+        crufilepath = homedir + '/.cruvis'
+    else:
+        raise('Operatingsystem unknown')
+    # Read path to data from config-file
+    config = configparser.ConfigParser()
+    config.read(crufilepath)
+    return config['PATH']['cru_dir']
+
+cru_dir = get_data_path()  
 cru_tmp_file = cru_dir + 'cru_ts4.01.1901.2016.tmp.dat.nc'
 cru_pre_file = cru_dir + 'cru_ts4.01.1901.2016.pre.dat.nc'
 cru_topo_file = cru_dir + 'cru_cl1_topography.nc'
